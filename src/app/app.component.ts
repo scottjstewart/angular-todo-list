c40738ca82180e4ccb1a7f05437868d735c66a1a
scottjstewart <scottstewart965@gmail.com>
2019-10-30 11:31:23 -0400

    finished tutorial set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-workout';
+  name: string = "Scott";
+  title: string = "IISD Home Workouts";
+
+  constructor() {
+    console.log("open");
+    this.changeName("Hannah");
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
